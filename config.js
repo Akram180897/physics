b781bb36e7385f7bab6a1339dfc3b9e683936b3a
Akram180897 <imakramsheikh@gmail.com>
2026-09-17 18:09:06 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 // Replace these two values with your Supabase Project URL and Publishable/anon key.
-window.SUPABASE_URL = "884c23be-b9ad-4173-80dd-83e8cdb2b7b3";
+window.SUPABASE_URL = "https://twzbxhhrtlbloytturea.supabase.co";
 window.SUPABASE_PUBLISHABLE_KEY = "sb_publishable_bSjU3iLvffKNySvSi69HFw_nLNOoJAc";
